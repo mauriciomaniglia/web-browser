0bbde18e92f5794d2a5cd03b58a3c0f8a9dfe464
mauriciomaniglia <manigliamauricio@gmail.com>
2024-05-30 22:30:59 -0300

    Create a new copy from WindowPresenter representing the iOS version

---
+++ web-browser/web-browser/iOS/Presentation/IOSWindowPresenter.swift
@@ -0,0 +1,107 @@
+import Foundation
+import core_web_browser
+
+class IOSWindowPresenter {
+    var didUpdatePresentableModel: ((WindowPresentableModel) -> Void)?
+    private var model: WindowPresentableModel
+
+    init() {
+        model = WindowPresentableModel(
+            urlHost: nil,
+            fullURL: nil,
+            showClearButton: false,
+            showStopButton: false,
+            showReloadButton: false,
+            showSiteProtection: false,
+            isWebsiteProtected: true,
+            showWebView: false,
+            canGoBack: false,
+            canGoForward: false)
+    }
+
+    func didStartNewWindow() {
+        didUpdatePresentableModel?(.init(
+            urlHost: nil,
+            fullURL: nil,
+            showClearButton: false,
+            showStopButton: false,
+            showReloadButton: false,
+            showSiteProtection: false,
+            isWebsiteProtected: true,
+            showWebView: false,
+            canGoBack: false,
+            canGoForward: false))
+    }
+
+    func didStartEditing() {
+        let newModel = WindowPresentableModel(
+            urlHost: model.urlHost,
+            fullURL: model.fullURL,
+            showClearButton: true,
+            showStopButton: false,
+            showReloadButton: false,
+            showSiteProtection: model.showSiteProtection,
+            isWebsiteProtected: model.isWebsiteProtected,
+            showWebView: model.showWebView,
+            canGoBack: model.canGoBack,
+            canGoForward: model.canGoForward)
+
+        model = newModel
+        didUpdatePresentableModel?(newModel)
+    }
+
+    func didEndEditing() {
+        let newModel = WindowPresentableModel(
+            urlHost: model.urlHost,
+            fullURL: model.fullURL,
+            showClearButton: false,
+            showStopButton: model.showStopButton,
+            showReloadButton: model.showReloadButton,
+            showSiteProtection: model.showSiteProtection,
+            isWebsiteProtected: model.isWebsiteProtected,
+            showWebView: model.showWebView,
+            canGoBack: model.canGoBack,
+            canGoForward: model.canGoForward)
+
+        model = newModel
+        didUpdatePresentableModel?(newModel)
+    }
+
+    func didLoadPage(url: URL, canGoBack: Bool, canGoForward: Bool) {
+        let fullURL = url.absoluteString
+        let urlHost = url.host ?? fullURL
+        let isOnSafelist = SafelistStore().isRegisteredDomain(urlHost)
+
+        let newModel = WindowPresentableModel(
+            urlHost: urlHost,
+            fullURL: fullURL,
+            showClearButton: false,
+            showStopButton: false,
+            showReloadButton: true,
+            showSiteProtection: true,
+            isWebsiteProtected: !isOnSafelist,
+            showWebView: true,
+            canGoBack: canGoBack,
+            canGoForward: canGoForward)
+
+        model = newModel
+        didUpdatePresentableModel?(newModel)
+    }
+
+    func didUpdateProgressBar(_ value: Double) {
+        let progressValue = value >= 1 ? nil : value
+
+        didUpdatePresentableModel?(.init(
+            urlHost: model.urlHost,
+            fullURL: model.fullURL,
+            showClearButton: false,
+            showStopButton: value < 1 ? true : false,
+            showReloadButton: value >= 1 ? true : false,
+            showSiteProtection: model.showSiteProtection,
+            isWebsiteProtected: model.isWebsiteProtected,
+            showWebView: true,
+            canGoBack: model.canGoBack,
+            canGoForward: model.canGoForward,
+            progressBarValue: progressValue))
+    }
+}
---
+++ web-browser/web-browserTests/iOS/Window/IOSWindowPresenterTests.swift
@@ -0,0 +1,230 @@
+import XCTest
+@testable import web_browser
+import core_web_browser
+
+class IOSWindowPresenterTests: XCTestCase {
+
+    func test_didStartNewWindow_deliversCorrectValues() {
+        let sut = IOSWindowPresenter()
+        var receivedResult: WindowPresentableModel?
+        sut.didUpdatePresentableModel = { receivedResult = $0 }
+
+        sut.didStartNewWindow()
+
+        XCTAssertNil(receivedResult!.urlHost)
+        XCTAssertNil(receivedResult!.fullURL)
+        XCTAssertFalse(receivedResult!.showClearButton)
+        XCTAssertFalse(receivedResult!.showStopButton)
+        XCTAssertFalse(receivedResult!.showReloadButton)
+        XCTAssertFalse(receivedResult!.showSiteProtection)
+        XCTAssertTrue(receivedResult!.isWebsiteProtected)
+        XCTAssertFalse(receivedResult!.showWebView)
+        XCTAssertFalse(receivedResult!.canGoBack)
+        XCTAssertFalse(receivedResult!.canGoForward)
+        XCTAssertNil(receivedResult!.progressBarValue)
+    }
+
+    func test_didStartNewWindow_deliversCorrectValuesWhenCallFromAnyPreviousState() {
+        let sut = IOSWindowPresenter()
+        var receivedResult: WindowPresentableModel?
+        sut.didUpdatePresentableModel = { receivedResult = $0 }
+
+        sut.didLoadPage(url: URL(string:"http://some-url.com/some-random-path/123")!, canGoBack: true, canGoForward: true)
+        sut.didStartNewWindow()
+
+        XCTAssertNil(receivedResult!.urlHost)
+        XCTAssertNil(receivedResult!.fullURL)
+        XCTAssertFalse(receivedResult!.showClearButton)
+        XCTAssertFalse(receivedResult!.showStopButton)
+        XCTAssertFalse(receivedResult!.showReloadButton)
+        XCTAssertFalse(receivedResult!.showSiteProtection)
+        XCTAssertTrue(receivedResult!.isWebsiteProtected)
+        XCTAssertFalse(receivedResult!.showWebView)
+        XCTAssertFalse(receivedResult!.canGoBack)
+        XCTAssertFalse(receivedResult!.canGoForward)
+        XCTAssertNil(receivedResult!.progressBarValue)
+    }
+
+    func test_didStartEditing_deliversCorrectValues() {
+        let sut = IOSWindowPresenter()
+        var receivedResult: WindowPresentableModel?
+        sut.didUpdatePresentableModel = { receivedResult = $0 }
+
+        sut.didStartEditing()
+
+        XCTAssertNil(receivedResult!.urlHost)
+        XCTAssertNil(receivedResult!.fullURL)
+        XCTAssertTrue(receivedResult!.showClearButton)
+        XCTAssertFalse(receivedResult!.showStopButton)
+        XCTAssertFalse(receivedResult!.showReloadButton)
+        XCTAssertFalse(receivedResult!.showSiteProtection)
+        XCTAssertTrue(receivedResult!.isWebsiteProtected)
+        XCTAssertFalse(receivedResult!.showWebView)
+        XCTAssertFalse(receivedResult!.canGoBack)
+        XCTAssertFalse(receivedResult!.canGoForward)
+        XCTAssertNil(receivedResult!.progressBarValue)
+    }
+
+    func test_didStartEditing_deliversCorrectValuesWithPageAlreadyLoaded() {
+        let sut = IOSWindowPresenter()
+        var receivedResult: WindowPresentableModel?
+        sut.didUpdatePresentableModel = { receivedResult = $0 }
+
+        sut.didLoadPage(url: URL(string:"http://some-url.com/some-random-path/123")!, canGoBack: true, canGoForward: true)
+        sut.didStartEditing()
+
+        XCTAssertEqual(receivedResult!.urlHost, "some-url.com")
+        XCTAssertEqual(receivedResult!.fullURL, "http://some-url.com/some-random-path/123")
+        XCTAssertTrue(receivedResult!.showClearButton)
+        XCTAssertFalse(receivedResult!.showStopButton)
+        XCTAssertFalse(receivedResult!.showReloadButton)
+        XCTAssertTrue(receivedResult!.showWebView)
+        XCTAssertTrue(receivedResult!.showSiteProtection)
+        XCTAssertTrue(receivedResult!.isWebsiteProtected)
+        XCTAssertTrue(receivedResult!.canGoBack)
+        XCTAssertTrue(receivedResult!.canGoForward)
+        XCTAssertNil(receivedResult!.progressBarValue)
+    }
+
+    func test_didEndEditing_deliversCorrectValues() {
+        let sut = IOSWindowPresenter()
+        var receivedResult: WindowPresentableModel?
+        sut.didUpdatePresentableModel = { receivedResult = $0 }
+
+        sut.didEndEditing()
+
+        XCTAssertNil(receivedResult!.urlHost)
+        XCTAssertNil(receivedResult!.fullURL)
+        XCTAssertFalse(receivedResult!.showClearButton)
+        XCTAssertFalse(receivedResult!.showStopButton)
+        XCTAssertFalse(receivedResult!.showReloadButton)
+        XCTAssertFalse(receivedResult!.showSiteProtection)
+        XCTAssertTrue(receivedResult!.isWebsiteProtected)
+        XCTAssertFalse(receivedResult!.showWebView)
+        XCTAssertFalse(receivedResult!.canGoBack)
+        XCTAssertFalse(receivedResult!.canGoForward)
+        XCTAssertNil(receivedResult!.progressBarValue)
+    }
+
+    func test_didEndEditing_deliversCorrectValuesWithPageAlreadyLoaded() {
+        let sut = IOSWindowPresenter()
+        var receivedResult: WindowPresentableModel?
+        sut.didUpdatePresentableModel = { receivedResult = $0 }
+
+        sut.didLoadPage(url: URL(string:"http://some-url.com/some-random-path/123")!, canGoBack: true, canGoForward: true)
+        sut.didEndEditing()
+
+        XCTAssertEqual(receivedResult!.urlHost, "some-url.com")
+        XCTAssertEqual(receivedResult!.fullURL, "http://some-url.com/some-random-path/123")
+        XCTAssertFalse(receivedResult!.showClearButton)
+        XCTAssertFalse(receivedResult!.showStopButton)
+        XCTAssertTrue(receivedResult!.showReloadButton)
+        XCTAssertTrue(receivedResult!.showSiteProtection)
+        XCTAssertTrue(receivedResult!.isWebsiteProtected)
+        XCTAssertTrue(receivedResult!.showWebView)
+        XCTAssertTrue(receivedResult!.canGoBack)
+        XCTAssertTrue(receivedResult!.canGoForward)
+        XCTAssertNil(receivedResult!.progressBarValue)
+    }
+
+    func test_didLoadPage_deliversCorrectValues() {
+        let sut = IOSWindowPresenter()
+        var receivedResult: WindowPresentableModel?
+        sut.didUpdatePresentableModel = { receivedResult = $0 }
+
+        sut.didLoadPage(url: URL(string:"http://some-url.com/some-random-path/123")!, canGoBack: true, canGoForward: true)
+
+        XCTAssertEqual(receivedResult!.urlHost, "some-url.com")
+        XCTAssertEqual(receivedResult!.fullURL, "http://some-url.com/some-random-path/123")
+        XCTAssertFalse(receivedResult!.showClearButton)
+        XCTAssertFalse(receivedResult!.showStopButton)
+        XCTAssertTrue(receivedResult!.showReloadButton)
+        XCTAssertTrue(receivedResult!.showSiteProtection)
+        XCTAssertTrue(receivedResult!.isWebsiteProtected)
+        XCTAssertTrue(receivedResult!.showWebView)
+        XCTAssertTrue(receivedResult!.canGoBack)
+        XCTAssertTrue(receivedResult!.canGoForward)
+        XCTAssertNil(receivedResult!.progressBarValue)
+    }
+
+    func test_didUpdateProgressBar_deliversCorrectValues() {
+        let sut = IOSWindowPresenter()
+        var receivedResult: WindowPresentableModel?
+        sut.didUpdatePresentableModel = { receivedResult = $0 }
+
+        sut.didUpdateProgressBar(0.45)
+
+        XCTAssertNil(receivedResult!.urlHost)
+        XCTAssertNil(receivedResult!.fullURL)
+        XCTAssertFalse(receivedResult!.showClearButton)
+        XCTAssertTrue(receivedResult!.showStopButton)
+        XCTAssertFalse(receivedResult!.showReloadButton)
+        XCTAssertFalse(receivedResult!.showSiteProtection)
+        XCTAssertTrue(receivedResult!.isWebsiteProtected)
+        XCTAssertTrue(receivedResult!.showWebView)
+        XCTAssertFalse(receivedResult!.canGoBack)
+        XCTAssertFalse(receivedResult!.canGoForward)
+        XCTAssertEqual(receivedResult!.progressBarValue, 0.45)
+    }
+
+    func test_didUpdateProgressBar_whenValueIsOne_stopButtonAndProgressBarShouldNotBeVisible() {
+        let sut = IOSWindowPresenter()
+        var receivedResult: WindowPresentableModel?
+        sut.didUpdatePresentableModel = { receivedResult = $0 }
+
+        sut.didUpdateProgressBar(1)
+
+        XCTAssertNil(receivedResult!.urlHost)
+        XCTAssertNil(receivedResult!.fullURL)
+        XCTAssertFalse(receivedResult!.showClearButton)
+        XCTAssertTrue(receivedResult!.showReloadButton)
+        XCTAssertFalse(receivedResult!.showSiteProtection)
+        XCTAssertTrue(receivedResult!.isWebsiteProtected)
+        XCTAssertTrue(receivedResult!.showWebView)
+        XCTAssertFalse(receivedResult!.canGoBack)
+        XCTAssertFalse(receivedResult!.canGoForward)
+        XCTAssertFalse(receivedResult!.showStopButton)
+        XCTAssertEqual(receivedResult!.progressBarValue, nil)
+    }
+
+    func test_didUpdateProgressBar_whenValueGreaterThanOne_stopButtonAndProgressBarShouldNotBeVisible() {
+        let sut = IOSWindowPresenter()
+        var receivedResult: WindowPresentableModel?
+        sut.didUpdatePresentableModel = { receivedResult = $0 }
+
+        sut.didUpdateProgressBar(1.5)
+
+        XCTAssertNil(receivedResult!.urlHost)
+        XCTAssertNil(receivedResult!.fullURL)
+        XCTAssertFalse(receivedResult!.showClearButton)
+        XCTAssertTrue(receivedResult!.showReloadButton)
+        XCTAssertFalse(receivedResult!.showSiteProtection)
+        XCTAssertTrue(receivedResult!.isWebsiteProtected)
+        XCTAssertTrue(receivedResult!.showWebView)
+        XCTAssertFalse(receivedResult!.canGoBack)
+        XCTAssertFalse(receivedResult!.canGoForward)
+        XCTAssertFalse(receivedResult!.showStopButton)
+        XCTAssertEqual(receivedResult!.progressBarValue, nil)
+    }
+
+    func test_didUpdateProgressBar_whenNavigationUpdates_deliversCorrectValues() {
+        let sut = IOSWindowPresenter()
+        var receivedResult: WindowPresentableModel?
+        sut.didUpdatePresentableModel = { receivedResult = $0 }
+
+        sut.didLoadPage(url: URL(string:"http://some-url.com/some-random-path/123")!, canGoBack: true, canGoForward: true)
+        sut.didUpdateProgressBar(0.45)
+
+        XCTAssertEqual(receivedResult!.urlHost, "some-url.com")
+        XCTAssertEqual(receivedResult!.fullURL, "http://some-url.com/some-random-path/123")
+        XCTAssertFalse(receivedResult!.showClearButton)
+        XCTAssertTrue(receivedResult!.showStopButton)
+        XCTAssertFalse(receivedResult!.showReloadButton)
+        XCTAssertTrue(receivedResult!.showSiteProtection)
+        XCTAssertTrue(receivedResult!.isWebsiteProtected)
+        XCTAssertTrue(receivedResult!.showWebView)
+        XCTAssertTrue(receivedResult!.canGoBack)
+        XCTAssertTrue(receivedResult!.canGoForward)
+        XCTAssertEqual(receivedResult!.progressBarValue, 0.45)
+    }
+}
