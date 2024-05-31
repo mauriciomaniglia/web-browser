c587ddd1c18d4617b50bd9607ed9c99fd93da0e6
mauriciomaniglia <manigliamauricio@gmail.com>
2024-05-31 09:11:23 -0300

    compose iOS window with new presenter and adapter components

--- web-browser/web-browser/Shared/Window/Composition/WindowComposer.swift
+++ web-browser/web-browser/Shared/Window/Composition/WindowComposer.swift
@@ -24,11 +24,40 @@ final class WindowComposer {
         windowPresenter.didUpdatePresentableModel = windowViewAdapter.updateViewModel
 
         #if os(iOS)
-        return IOSWindow(viewModel: viewModel, webView: AnyView(WebViewUIKitWrapper(webView: webKitEngineWrapper.webView)))
+        return composeIOSWindow()
         #elseif os(macOS)
         return MacOSWindow(viewModel: viewModel, webView: AnyView(WebViewAppKitWrapper(webView: webKitEngineWrapper.webView)))
         #elseif os(visionOS)
         return VisionOSWindow(viewModel: viewModel, webView: AnyView(WebViewUIKitWrapper(webView: webKitEngineWrapper.webView)))
         #endif
     }
+
+    #if os(iOS)
+    private func composeIOSWindow() -> IOSWindow {
+        let webKitEngineWrapper = WebKitEngineWrapper()
+        let windowPresenter = IOSWindowPresenter()
+        let safelistStore = SafelistStore()
+        let viewModel = WindowViewModel()
+        let windowViewAdapter = IOSWindowViewAdapter(
+            webView: webKitEngineWrapper,
+            presenter: windowPresenter,
+            safelist: safelistStore,
+            viewModel: viewModel)
+
+        viewModel.didTapBackButton = windowViewAdapter.didTapBackButton
+        viewModel.didTapForwardButton = windowViewAdapter.didTapForwardButton
+        viewModel.didTapCancelButton = windowViewAdapter.didEndEditing
+        viewModel.didReload = windowViewAdapter.didReload
+        viewModel.didStopLoading = windowViewAdapter.didStopLoading
+        viewModel.didStartSearch = windowViewAdapter.didRequestSearch
+        viewModel.didUpdateSafelist = windowViewAdapter.updateSafelist(url:isEnabled:)
+        viewModel.didBeginEditing = windowViewAdapter.didStartEditing
+        viewModel.didEndEditing = windowViewAdapter.didEndEditing
+
+        webKitEngineWrapper.delegate = windowViewAdapter
+        windowPresenter.didUpdatePresentableModel = windowViewAdapter.updateViewModel
+
+        return IOSWindow(viewModel: viewModel, webView: AnyView(WebViewUIKitWrapper(webView: webKitEngineWrapper.webView)))
+    }
+    #endif
 }
