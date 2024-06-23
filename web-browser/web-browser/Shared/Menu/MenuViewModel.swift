fd3d65248cdae9a3192be3d3de2a536a28146b8c
mauriciomaniglia <manigliamauricio@gmail.com>
2024-06-22 21:26:54 -0300

    Create HistoryPage representation in MenuViewModel

--- web-browser/web-browser/Shared/Menu/MenuViewModel.swift
+++ web-browser/web-browser/Shared/Menu/MenuViewModel.swift
@@ -2,7 +2,13 @@ import Foundation
 
 class MenuViewModel: ObservableObject {
 
+    struct HistoryPage {
+        let title: String
+        let url: URL
+    }
+
     @Published var showMenu: Bool = false
+    @Published var historyList: [HistoryPage] = []
 
     var didTapMenuButton: (() -> Void)?
     var didTapHistoryOption: (() -> Void)?
