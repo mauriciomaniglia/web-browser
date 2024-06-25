f70493cc36d3ca99a48030826d6f1319ee1673cc
mauriciomaniglia <manigliamauricio@gmail.com>
2024-06-25 07:57:13 -0300

    Create history view

---
+++ web-browser/web-browser/Shared/Menu/HistoryView.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct HistoryView: View {
+    var didSelectHistory: ((Int) -> Void)?
+    let historyList: [MenuViewModel.HistoryPage]
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(historyList.indices, id: \.self) { index in
+                let item = historyList[index]
+
+                HStack {
+                    Text("\(item.title)")
+                    Spacer()
+                }
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    didSelectHistory?(index)
+                }
+
+                if index < historyList.count-1 {
+                    Divider()
+                }
+            }
+            .presentationCompactAdaptation((.popover))
+        }
+        .padding()
+    }
+}
