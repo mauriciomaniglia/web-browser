a4b6a4b8db21e7f8c3d5f07ff97bc7514ef41488
mauriciomaniglia <manigliamauricio@gmail.com>
2024-06-29 19:19:25 -0300

    Create helper function makeSUT to improve tests

--- web-browser/web-browserTests/Menu/MenuPresenterTests.swift
+++ web-browser/web-browserTests/Menu/MenuPresenterTests.swift
@@ -5,8 +5,7 @@ import core_web_browser
 class MenuPresenterTests: XCTestCase {
 
     func test_didOpenMenuView_deliversCorrectModel() {
-        let history = HistoryStoreMock()
-        let sut = MenuPresenter(history: history)
+        let (sut, _) = makeSUT()
 
         let model = sut.didOpenMenuView()
 
@@ -15,12 +14,11 @@ class MenuPresenterTests: XCTestCase {
     }
 
     func test_didOpenHistoryView_deliversCorrectModel() {
-        let history = HistoryStoreMock()
+        let (sut, history) = makeSUT()
         let page1 = WebPage(title: "title 1", url: URL(string: "http://page1.com")!)
         let page2 = WebPage(title: "", url: URL(string: "http://page2.com")!)
         let page3 = WebPage(title: nil, url: URL(string: "http://page3.com")!)
         history.mockWebPages = [page1, page2, page3]
-        let sut = MenuPresenter(history: history)
 
         let model = sut.didOpenHistoryView()
 
@@ -31,6 +29,15 @@ class MenuPresenterTests: XCTestCase {
         XCTAssertEqual(model.historyList?[1].url, URL(string:"http://page2.com")!)
         XCTAssertEqual(model.historyList?[2].title, "http://page3.com")
         XCTAssertEqual(model.historyList?[2].url, URL(string:"http://page3.com")!)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT() -> (sut: MenuPresenter, historyMock: HistoryStoreMock) {
+        let history = HistoryStoreMock()
+        let sut = MenuPresenter(history: history)
+
+        return (sut, history)
     }
 }
 
