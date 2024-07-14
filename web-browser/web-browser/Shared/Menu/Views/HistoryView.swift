cbf81ea5f838bbf0d0b68330fd0506e785c7ea62
mauriciomaniglia <manigliamauricio@gmail.com>
2024-07-14 13:06:18 -0300

    Add history header view

--- web-browser/web-browser/Shared/Menu/Views/HistoryView.swift
+++ web-browser/web-browser/Shared/Menu/Views/HistoryView.swift
@@ -4,9 +4,28 @@ struct HistoryView: View {
     var didSelectPage: ((MenuViewModel.HistoryPage) -> Void)?
     let pages: [MenuViewModel.HistoryPage]
 
+    @State private var searchText: String = ""
+
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
+                VStack {
+                    Text("History")
+
+                    HStack {
+                        TextField("Search History", text: $searchText)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding()
+
+                        Button(action: {}) {
+                            Text("Cancel")
+                        }
+                    }
+                    .cornerRadius(10)
+                }
+
+                Spacer()
+
                 ForEach(pages.indices, id: \.self) { index in
                     let item = pages[index]
 
