ca4723a627424e0f154aab62a5306ab05611a6aa
mauriciomaniglia <manigliamauricio@gmail.com>
2024-07-17 21:01:46 -0300

    Create HistoryComposer component

---
+++ web-browser/web-browser/Shared/History/Composition/HistoryComposer.swift
@@ -0,0 +1,18 @@
+import core_web_browser
+
+class HistoryComposer {
+
+    func makeHistoryViewModel(webView: WebEngineContract) -> HistoryViewModel {
+        let viewModel = HistoryViewModel()
+        let historyStore = HistoryStore()
+        let presenter = HistoryPresenter(history: historyStore)
+        let adapter = HistoryAdapter(
+            viewModel: viewModel,
+            presenter: presenter,
+            webView: webView)
+        viewModel.didSelectPageHistory = adapter.didSelectPageHistory(_:)
+        presenter.didUpdatePresentableModel = adapter.updateViewModel
+
+        return viewModel
+    }
+}
