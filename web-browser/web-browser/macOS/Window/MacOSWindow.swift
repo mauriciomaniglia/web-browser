4b93d877d66b458d98bf8b585fd0c1fabcfc3885
mauriciomaniglia <manigliamauricio@gmail.com>
2024-08-13 20:52:49 -0300

    Add Sidebar menu for macOS

--- web-browser/web-browser/macOS/Window/MacOSWindow.swift
+++ web-browser/web-browser/macOS/Window/MacOSWindow.swift
@@ -7,19 +7,34 @@ struct MacOSWindow: View {
     let webView: AnyView
 
     var body: some View {
-        VStack {
-            HStack {
-                WindowNavigationButtons(viewModel: windowViewModel)
-                AddressBarView(viewModel: windowViewModel)
-                Spacer()
-                if windowViewModel.showMenuButton {
-                    MenuButton(viewModel: menuViewModel)
+        NavigationSplitView {
+            Sidebar()
+        } detail: {
+            VStack {
+                HStack {
+                    WindowNavigationButtons(viewModel: windowViewModel)
+                    AddressBarView(viewModel: windowViewModel)
+                    Spacer()
+                    if windowViewModel.showMenuButton {
+                        MenuButton(viewModel: menuViewModel)
+                    }
                 }
+                Spacer()
+                webView
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
-            Spacer()
-            webView
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
         }
-        .padding()
+    }
+}
+
+struct Sidebar: View {
+    var body: some View {
+        List {
+            NavigationLink(value: "History") {
+                Label("History", systemImage: "clock.arrow.circlepath")
+            }
+        }
+        .navigationSplitViewColumnWidth(min: 200, ideal: 200)
     }
 }
