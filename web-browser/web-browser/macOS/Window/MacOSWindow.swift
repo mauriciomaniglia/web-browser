90c5e42036228356f709ec8f354e7a2ec3cabcf3
mauriciomaniglia <manigliamauricio@gmail.com>
2024-08-20 23:46:48 -0300

    Move Sidebar to its own file

--- web-browser/web-browser/macOS/Window/MacOSWindow.swift
+++ web-browser/web-browser/macOS/Window/MacOSWindow.swift
@@ -27,16 +27,3 @@ struct MacOSWindow: View {
         }
     }
 }
-
-struct Sidebar: View {
-    @ObservedObject var menuViewModel: MenuViewModel
-
-    var body: some View {
-        List {
-            NavigationLink(destination: HistoryView(viewModel: menuViewModel.historyViewModel)) {
-                Label("History", systemImage: "clock.arrow.circlepath")
-            }
-        }
-        .navigationSplitViewColumnWidth(min: 200, ideal: 200)
-    }
-}
