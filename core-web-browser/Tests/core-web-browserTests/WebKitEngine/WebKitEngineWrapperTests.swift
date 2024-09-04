ebed24fc687f75a2360f1b1f578f8ffc2850a451
mauriciomaniglia <manigliamauricio@gmail.com>
2024-09-03 21:02:07 -0300

    Move WebViewProxyDelegateSpy to its own file

--- core-web-browser/Tests/core-web-browserTests/WebKitEngine/WebKitEngineWrapperTests.swift
+++ core-web-browser/Tests/core-web-browserTests/WebKitEngine/WebKitEngineWrapperTests.swift
@@ -158,27 +158,5 @@ class WebKitEngineWrapperTests: XCTestCase {
         return (sut, webView, ruleStore, delegate)
     }
 
-    private class WKWebViewConfigurationDummy: WKWebViewConfiguration {}
-
-    private class WebViewProxyDelegateSpy: WebEngineDelegate {
-        enum Message: Equatable {
-            case didLoadPage
-            case didUpdateLoadingProgress(_: Double)
-            case didUpdateNavigationButtons
-        }
-
-        var receivedMessages = [Message]()
-
-        func didLoad(page: WebPage) {
-            receivedMessages.append(.didLoadPage)
-        }
-
-        func didUpdateNavigationButtons(canGoBack: Bool, canGoForward: Bool) {
-            receivedMessages.append(.didUpdateNavigationButtons)
-        }
-
-        func didUpdateLoadingProgress(_ progress: Double) {
-            receivedMessages.append(.didUpdateLoadingProgress(progress))
-        }
-    }
+    private class WKWebViewConfigurationDummy: WKWebViewConfiguration {}    
 }
