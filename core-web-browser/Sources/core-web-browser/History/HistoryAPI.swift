e59827fee0cb17746789cf7d3859f28bc3644e54
mauriciomaniglia <manigliamauricio@gmail.com>
2024-09-20 08:21:12 -0300

    Add new method deletePages into HistoryAPI

--- core-web-browser/Sources/core-web-browser/History/HistoryAPI.swift
+++ core-web-browser/Sources/core-web-browser/History/HistoryAPI.swift
@@ -1,5 +1,8 @@
+import Foundation
+
 public protocol HistoryAPI {
-    func save(page: WebPage)    
+    func save(page: WebPage)
     func getPages() -> [WebPage]
     func getPages(by searchTerm: String) -> [WebPage]
+    func deletePages(withIDs ids: [UUID])
 }
