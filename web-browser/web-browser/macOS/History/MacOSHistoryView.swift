da1e9a9f670855545237908b1bf80838f1189cd8
mauriciomaniglia <manigliamauricio@gmail.com>
2024-10-01 22:04:44 -0300

    Code refactor

--- web-browser/web-browser/macOS/History/MacOSHistoryView.swift
+++ web-browser/web-browser/macOS/History/MacOSHistoryView.swift
@@ -7,13 +7,24 @@ struct MacOSHistoryView: View {
     @State private var searchText: String = ""
 
     var body: some View {
+        searchTopBar
+        if viewModel.selectedPages.count > 0 {
+            selectedPagesView
+        }
+        historyList
+        .navigationTitle("History")
+        .onAppear {
+            viewModel.didOpenHistoryView?()
+        }
+    }
+
+    var searchTopBar: some View {
         HStack {
             Button {
                 dismiss()
             } label: {
                 Image(systemName: "arrow.left")
             }
-
             TextField("Search History", text: $searchText)
                 .onChange(of: searchText, { _, newValue in
                     viewModel.didSearchTerm?(newValue)
@@ -22,56 +33,43 @@ struct MacOSHistoryView: View {
                 .padding()
         }
         .padding()
+    }
 
-        if viewModel.selectedPages.count > 0 {
-            HStack {
-                Button {
-                    viewModel.deselectAllPages()
-                } label: {
-                    Image(systemName: "xmark")
-                }
-
-                Text("\(viewModel.selectedPages.count) selected")
-
-                Spacer()
-
-                Button {
-                    viewModel.deleteSelectedPages()
-                } label: {
-                    Text("Delete")
-                }
+    var selectedPagesView: some View {
+        HStack {
+            Button {
+                viewModel.deselectAllPages()
+            } label: {
+                Image(systemName: "xmark")
             }
-            .padding()
+            Text("\(viewModel.selectedPages.count) selected")
+            Spacer()
+            Button {
+                viewModel.deleteSelectedPages()
+            } label: {
+                Text("Delete")
+            }
         }
+        .padding()
+    }
 
+    var historyList: some View {
         List {
             ForEach(viewModel.historyList.indices, id: \.self) { sectionIndex in
                 let item = viewModel.historyList[sectionIndex]
-
                 let header = Text(item.title)
-
                 Section(header: header) {
                     ForEach(item.pages.indices, id: \.self) { pageIndex in
                         let page = item.pages[pageIndex]
-
                         Toggle(isOn: $viewModel.historyList[sectionIndex].pages[pageIndex].isSelected) {
                             Text(page.title)
                         }
                         .toggleStyle(CheckboxToggleStyle())
                         .padding()
                     }
-                    .onDelete(perform: delete)
                 }
             }
         }
-        .navigationTitle("History")
-        .onAppear {
-            viewModel.didOpenHistoryView?()
-        }
-    }
-
-    func delete(at offsets: IndexSet) {
-
     }
 }
 #endif
