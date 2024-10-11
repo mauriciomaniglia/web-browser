10762c3a35234564e1be5a3050aadd071336aad2
mauriciomaniglia <manigliamauricio@gmail.com>
2024-10-11 08:30:29 -0300

    Add CommandMenu option for bookmarks

--- web-browser/web-browser/WebBrowserApp.swift
+++ web-browser/web-browser/WebBrowserApp.swift
@@ -6,5 +6,26 @@ struct WebBrowserApp: App {
         WindowGroup {
             ContentView()
         }
+        .commands {
+            CommandMenu("Bookmarks") {
+                Button("Add Bookmark") {
+                    addBookmark()
+                }
+                .keyboardShortcut("D", modifiers: .command)
+
+                Button("Show Bookmarks") {
+                    showBookmarks()
+                }
+                .keyboardShortcut("B", modifiers: [.command, .shift])
+            }
+        }
+    }
+
+    private func addBookmark() {
+        print("Add Bookmark")
+    }
+
+    private func showBookmarks() {
+        print("Show Bookmarks")
     }
 }
