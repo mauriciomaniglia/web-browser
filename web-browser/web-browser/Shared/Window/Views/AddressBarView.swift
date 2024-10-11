6e4900025a7464d0a36a2cd71653048f3fc97388
mauriciomaniglia <manigliamauricio@gmail.com>
2024-10-11 08:14:46 -0300

    Improve code structure on AddressBarView

--- web-browser/web-browser/Shared/Window/Views/AddressBarView.swift
+++ web-browser/web-browser/Shared/Window/Views/AddressBarView.swift
@@ -14,39 +14,19 @@ struct AddressBarView: View {
                             WebsiteProtectionButton(viewModel: viewModel, isShowingSheet: isShowingSheet)
                         }
 
-                        TextField("Search or enter address", text: $viewModel.fullURL)
-                            .textFieldStyle(.plain)
-                            .onSubmit { viewModel.didStartSearch?(viewModel.fullURL) }
-                            .focused($isTextFieldFocused)
-                            .onChange(of: isTextFieldFocused) { _, isFocused in
-                                if isFocused {
-                                    viewModel.didBeginEditing?()
-                                } else {
-                                    viewModel.didEndEditing?()
-                                }
-                            }
+                        SearchTextField
 
                         if viewModel.showStopButton {
-                            Button(action: { viewModel.didStopLoading?() }) {
-                                Image(systemName: "xmark")
-                            }
-                            .buttonStyle(PlainButtonStyle())
+                            StopButton
                         }
 
                         if viewModel.showReloadButton {
-                            Button(action: { viewModel.didReload?() }) {
-                                Image(systemName: "goforward")
-                            }
-                            .buttonStyle(PlainButtonStyle())
+                            ReloadButton
                         }
 
                         if viewModel.showClearButton {
-                            Button(action: { viewModel.fullURL = "" }) {
-                                Image(systemName: "xmark.circle")
-                            }
-                            .buttonStyle(PlainButtonStyle())
+                            ClearButton
                         }
-
                     }
                     .padding(.horizontal)
                 }
@@ -54,17 +34,13 @@ struct AddressBarView: View {
                 .background(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
 
                 if viewModel.showCancelButton {
-                    Button(action: { didTapCancelButton() }) {
-                        Text("Cancel")
-                    }
-                    .buttonStyle(PlainButtonStyle())
+                    CancelButton
                 }
             }
             .padding(.horizontal)
 
             if (viewModel.progressBarValue != nil) {
-                ProgressView(value: viewModel.progressBarValue, total: 1.0)
-                    .padding(EdgeInsets(top: 0, leading: 18, bottom: 0, trailing: 18))
+                ProgressBar
             }
         }
         .onAppear {
@@ -72,6 +48,53 @@ struct AddressBarView: View {
                 isTextFieldFocused = false
             }
         }
+    }
+
+    private var SearchTextField: some View {
+        TextField("Search or enter address", text: $viewModel.fullURL)
+            .textFieldStyle(.plain)
+            .onSubmit { viewModel.didStartSearch?(viewModel.fullURL) }
+            .focused($isTextFieldFocused)
+            .onChange(of: isTextFieldFocused) { _, isFocused in
+                if isFocused {
+                    viewModel.didBeginEditing?()
+                } else {
+                    viewModel.didEndEditing?()
+                }
+            }
+    }
+
+    private var StopButton: some View {
+        Button(action: { viewModel.didStopLoading?() }) {
+            Image(systemName: "xmark")
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+
+    private var ReloadButton: some View {
+        Button(action: { viewModel.didReload?() }) {
+            Image(systemName: "goforward")
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+
+    private var ClearButton: some View {
+        Button(action: { viewModel.fullURL = "" }) {
+            Image(systemName: "xmark.circle")
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+
+    private var CancelButton: some View {
+        Button(action: { didTapCancelButton() }) {
+            Text("Cancel")
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+
+    private var ProgressBar: some View {
+        ProgressView(value: viewModel.progressBarValue, total: 1.0)
+            .padding(EdgeInsets(top: 0, leading: 18, bottom: 0, trailing: 18))
     }
 
     private func didTapCancelButton() {
