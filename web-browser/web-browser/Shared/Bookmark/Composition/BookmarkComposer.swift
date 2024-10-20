1a895073699761be26063d95f9dc2edf2c6395b8
mauriciomaniglia <manigliamauricio@gmail.com>
2024-10-20 08:44:20 -0300

    Create new component BookmarkComposer

---
+++ web-browser/web-browser/Shared/Bookmark/Composition/BookmarkComposer.swift
@@ -0,0 +1,20 @@
+import Core
+
+class BookmarkComposer {
+
+    func makeBookmarkViewModel(webView: WebEngineContract) -> BookmarkViewModel {
+        let viewModel = BookmarkViewModel()
+        let bookmarkStore = BookmarkSwiftDataStore()
+        let presenter = BookmarkPresenter()
+        let adapter = BookmarkAdapter(viewModel: viewModel)
+        let facade = BookmarkFacade(presenter: presenter, webView: webView, bookmark: bookmarkStore)
+
+        viewModel.didSelectPage = facade.didSelectPage(_:)
+        viewModel.didOpenBookmarkView = facade.didOpenBookmarkView
+        viewModel.didSearchTerm = facade.didSearchTerm(_:)
+        viewModel.didTapDeletePages = facade.didTapDeletePages
+        presenter.didUpdatePresentableModels = adapter.updateViewModel
+
+        return viewModel
+    }
+}
