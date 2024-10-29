8a5e5ac1eb358e83874941fa28abe8344b799b26
mauriciomaniglia <manigliamauricio@gmail.com>
2024-10-29 10:30:38 -0300

    Add new menu item Add Bookmark for iOS

--- web-browser/web-browser/iOS/Menu/IOSMenuView.swift
+++ web-browser/web-browser/iOS/Menu/IOSMenuView.swift
@@ -8,8 +8,18 @@ struct IOSMenuView: View {
     var body: some View {
         NavigationView {
             List {
+                Button(action: {                    
+                    print("Performing custom action for Add Bookmark")
+                }) {
+                    HStack {
+                        Label("Add Bookmark", systemImage: "bookmark")
+                        Spacer()
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
+
                 NavigationLink(destination: IOSBookmarkView(viewModel: windowViewModel.bookmarkViewModel)) {
-                    Label("Bookmarks", systemImage: "bookmark")
+                    Label("Bookmarks", systemImage: "book")
                 }
                 NavigationLink(destination: IOSHistoryView(viewModel: windowViewModel.historyViewModel, isPresented: $isPresented)) {
                     Label("History", systemImage: "clock.arrow.circlepath")
