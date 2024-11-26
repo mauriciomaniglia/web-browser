4e3eee94458ae7f8e247b0a8e66e5b8fb572c2b9
mauriciomaniglia <manigliamauricio@gmail.com>
2024-11-26 11:33:07 -0300

    Move HistoryStoreSpy to its own file

---
+++ web-browser/web-browserTests/Helpers/HistoryStoreSpy.swift
@@ -0,0 +1,35 @@
+import Foundation
+@testable import web_browser
+@testable import Services
+
+class HistoryStoreSpy: HistoryAPI {
+    enum Message: Equatable {
+        case save(_ url: URL)
+        case getPages
+        case deletePages
+        case deleteAllPages
+    }
+
+    var receivedMessages = [Message]()
+
+    func save(_ page: WebPage) {
+        receivedMessages.append(.save(page.url))
+    }
+
+    func getPages() -> [WebPage] {
+        receivedMessages.append(.getPages)
+        return []
+    }
+
+    func getPages(by searchTerm: String) -> [WebPage] {
+        return []
+    }
+
+    func deletePages(withIDs ids: [UUID]) {
+        receivedMessages.append(.deletePages)
+    }
+
+    func deleteAllPages() {
+        receivedMessages.append(.deleteAllPages)
+    }
+}
--- web-browser/web-browserTests/Window/WindowViewAdapterTests.swift
+++ web-browser/web-browserTests/Window/WindowViewAdapterTests.swift
@@ -87,38 +87,6 @@ class WindowViewAdapterTests: XCTestCase {
     }
 }
 
-private class HistoryStoreSpy: HistoryAPI {
-    enum Message: Equatable {
-        case save(_ url: URL)
-        case getPages
-        case deletePages
-        case deleteAllPages
-    }
-
-    var receivedMessages = [Message]()
-
-    func save(_ page: WebPage) {
-        receivedMessages.append(.save(page.url))
-    }
-
-    func getPages() -> [WebPage] {
-        receivedMessages.append(.getPages)
-        return []
-    }
-
-    func getPages(by searchTerm: String) -> [WebPage] {
-        return []
-    }
-
-    func deletePages(withIDs ids: [UUID]) {
-        receivedMessages.append(.deletePages)
-    }
-
-    func deleteAllPages() {
-        receivedMessages.append(.deleteAllPages)
-    }
-}
-
 class SafelistStoreSpy: SafelistAPI {
     enum Message: Equatable {
         case isRegisteredDomain(_ domain: String)
