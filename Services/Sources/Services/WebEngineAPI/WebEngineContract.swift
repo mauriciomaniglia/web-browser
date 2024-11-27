3ce77b4f4c22e2fb6ebe0e4f30bac628145e24ff
mauriciomaniglia <manigliamauricio@gmail.com>
2024-11-27 10:18:33 -0300

    Move WebPage to its own file

--- Services/Sources/Services/WebEngineAPI/WebEngineContract.swift
+++ Services/Sources/Services/WebEngineAPI/WebEngineContract.swift
@@ -1,19 +1,5 @@
 import Foundation
 
-public struct WebPage {
-    public let id: UUID
-    public let title: String?
-    public let url: URL
-    public let date: Date
-
-    public init(id: UUID = UUID(), title: String?, url: URL, date: Date) {
-        self.id = id
-        self.title = title
-        self.url = url
-        self.date = date
-    }
-}
-
 public protocol WebEngineContract {
     func getCurrentPage() -> WebPage?
     func registerRule(name: String, content: String, safelist: [String])
