d0e79f5b8fd7e1a73bae6e5a345d46df46929d65
mauriciomaniglia <manigliamauricio@gmail.com>
2025-05-28 09:25:29 -0300

    Create a contract protocol for SearchAPI

--- Services/Sources/Services/Search/SearchAPI.swift
+++ Services/Sources/Services/Search/SearchAPI.swift
@@ -1,6 +1,10 @@
 import Foundation
 
-public final class SearchAPI {
+protocol SearchAPIContract {
+    static func makeURL(from text: String) -> URL
+}
+
+public final class SearchAPI: SearchAPIContract {
 
     public static func makeURL(from text: String) -> URL {
         if let url = URIFixup.getURL(text) {
