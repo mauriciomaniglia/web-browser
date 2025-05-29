5c8fbe374d95f19413badd49aa9452f850701836
mauriciomaniglia <manigliamauricio@gmail.com>
2025-05-29 19:12:53 -0300

    Add new function getSuggestions to SearchAPI

--- Services/Sources/Services/Search/SearchAPI.swift
+++ Services/Sources/Services/Search/SearchAPI.swift
@@ -2,15 +2,35 @@ import Foundation
 
 protocol SearchAPIContract {
     static func makeURL(from text: String) -> URL
+    func getSuggestions(from text: String, callback: @escaping ([String]) -> Void)
 }
 
 public final class SearchAPI: SearchAPIContract {
+
+    let searchSuggestionService: SearchSuggestionServiceContract
+
+    init(searchSuggestionService: SearchSuggestionServiceContract) {
+        self.searchSuggestionService = searchSuggestionService
+    }
 
     public static func makeURL(from text: String) -> URL {
         if let url = URIFixup.getURL(text) {
             return url
         } else {
             return SearchEngineURLBuilder.buildSearchURL(query: text)
+        }
+    }
+
+    public func getSuggestions(from text: String, callback: @escaping ([String]) -> Void) {
+        let url = SearchEngineURLBuilder.buildAutocompleteURL(query: text)
+
+        searchSuggestionService.query(url) { suggestions in
+            if var suggestions {
+                suggestions.insert(text, at: 0)
+                callback(suggestions)
+            } else {
+                callback([text])
+            }
         }
     }
 }
--- Services/Tests/ServicesTests/Search/SearchAPITests.swift
+++ Services/Tests/ServicesTests/Search/SearchAPITests.swift
@@ -1,6 +1,6 @@
 import Foundation
 import XCTest
-import Services
+@testable import Services
 
 class SearchAPITests: XCTestCase {
     func test_makeURL_withCorrectURLText_deliversURL() {
@@ -13,5 +13,47 @@ class SearchAPITests: XCTestCase {
         let url = SearchAPI.makeURL(from: "apple")
 
         XCTAssertEqual(url.absoluteString, "https://www.google.com/search?q=apple&ie=utf-8&oe=utf-8")
+    }
+
+    func test_getSuggestion_whenThereIsNoSuggestion_deliversInputQuery() {
+        let service = MockSearchSuggestionService()
+        let sut = SearchAPI(searchSuggestionService: service)
+        var receivedSuggestions: [String]?
+
+        sut.getSuggestions(from: "apple") { suggestions in
+            receivedSuggestions = suggestions
+        }
+        service.simulateResponseWithNoSuggestions()
+
+        XCTAssertEqual(receivedSuggestions, ["apple"])
+    }
+
+    func test_getSuggestion_whenThereIsSuggestion_deliversInputQueryAndSuggestions() {
+        let service = MockSearchSuggestionService()
+        let sut = SearchAPI(searchSuggestionService: service)
+        var receivedSuggestions: [String]?
+
+        sut.getSuggestions(from: "apple") { suggestions in
+            receivedSuggestions = suggestions
+        }
+        service.simulateResponseWithSuggestions(["apple watch", "apple tv", "apple music"])
+
+        XCTAssertEqual(receivedSuggestions, ["apple", "apple watch", "apple tv", "apple music"])
+    }
+}
+
+private class MockSearchSuggestionService: SearchSuggestionServiceContract {
+    var receivedCallback: SearchSuggestionService.SearchSuggestionResponse?
+
+    func query(_ url: URL, callback: @escaping SearchSuggestionService.SearchSuggestionResponse) {
+        receivedCallback = callback
+    }
+
+    func simulateResponseWithNoSuggestions() {
+        receivedCallback?(nil)
+    }
+
+    func simulateResponseWithSuggestions(_ suggestions: [String]) {
+        receivedCallback?(suggestions)
     }
 }
