b5fccdeb2f1136caf072b2958b05255deab356e3
mauriciomaniglia <manigliamauricio@gmail.com>
2025-06-10 22:02:18 -0300

    Fix bookmark mediator test name

--- Services/Tests/ServicesTests/Bookmark/BookmarkMediatorTests.swift
+++ Services/Tests/ServicesTests/Bookmark/BookmarkMediatorTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import Services
 
-class BookmarkFacadeTests: XCTestCase {
+class BookmarkMediatorTests: XCTestCase {
 
     func test_didOpenBookmarkView_sendsCorrectMessage() {
         let (sut, presenter,webView, bookmarkStore) = makeSUT()
