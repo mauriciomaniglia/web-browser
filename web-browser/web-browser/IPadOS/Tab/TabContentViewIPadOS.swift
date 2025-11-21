f8b5295a6a1361949b65540fb4e3e77127e19f21
mauriciomaniglia <manigliamauricio@gmail.com>
2025-11-21 16:33:19 -0300

    Extract code WindowIPad and create a new component TabContentViewIPadOS

---
+++ web-browser/web-browser/IPadOS/Tab/TabContentViewIPadOS.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct TabContentViewIPadOS: View {
+    @ObservedObject var windowViewModel: WindowViewModel
+
+    let webView: AnyView
+
+    var body: some View {
+        VStack {
+            webView
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .opacity(windowViewModel.showWebView ? 1 : 0)
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                WindowNavigationButtons(viewModel: windowViewModel)
+            }
+            ToolbarItem(placement: .principal) {
+                AddressBarView(viewModel: windowViewModel, searchText: $windowViewModel.fullURL)
+                    .popover(isPresented: $windowViewModel.showSearchSuggestions, attachmentAnchor: .point(.bottom)) {
+                        SearchSuggestionView(viewModel: windowViewModel.searchSuggestionViewModel)
+                            .frame(width: 550)
+                    }
+            }
+            if let url = URL(string: windowViewModel.fullURL) {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    ShareLink(item: url) {
+                        Image(systemName: "square.and.arrow.up")
+                    }
+                }
+            }
+        }
+    }
+}
--- web-browser/web-browser/IPadOS/Window/WindowIPad.swift
+++ web-browser/web-browser/IPadOS/Window/WindowIPad.swift
@@ -11,30 +11,7 @@ struct WindowIPadOS: View {
             NavigationSplitView {
                 MenuIPadOS(windowViewModel: windowViewModel)
             } detail: {
-                VStack {
-                    webView
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .opacity(windowViewModel.showWebView ? 1 : 0)
-                }
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        WindowNavigationButtons(viewModel: windowViewModel)
-                    }
-                    ToolbarItem(placement: .principal) {
-                        AddressBarView(viewModel: windowViewModel, searchText: $windowViewModel.fullURL)
-                            .popover(isPresented: $windowViewModel.showSearchSuggestions, attachmentAnchor: .point(.bottom)) {
-                                SearchSuggestionView(viewModel: windowViewModel.searchSuggestionViewModel)
-                                    .frame(width: 550)
-                            }
-                    }
-                    if let url = URL(string: windowViewModel.fullURL) {
-                        ToolbarItem(placement: .navigationBarTrailing) {
-                            ShareLink(item: url) {
-                                Image(systemName: "square.and.arrow.up")
-                            }
-                        }
-                    }
-                }
+                TabContentViewIPadOS(windowViewModel: windowViewModel, webView: webView)
             }
         }
         .overlay(alignment: .center) {
