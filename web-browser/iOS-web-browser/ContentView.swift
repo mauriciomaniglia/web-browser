3091bba74c36519d157e830309f18cdf43022119
mauriciomaniglia <manigliamauricio@gmail.com>
2025-12-14 09:50:33 -0300

    Create new target for iOS

---
+++ web-browser/iOS-web-browser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-web-browser
+//
+//  Created by Mauricio Cesar on 14/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ web-browser/iOS-web-browser/iOS_web_browserApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_web_browserApp.swift
+//  iOS-web-browser
+//
+//  Created by Mauricio Cesar on 14/12/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_web_browserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
