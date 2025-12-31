2de3d93adc1e84d2907dfcd15e29da1724a3bf6c
mauriciomaniglia <manigliamauricio@gmail.com>
2025-12-31 15:36:19 -0300

    Refactor code structure for AddBookmark on iOS

--- web-browser/iOS-web-browser/Bookmark/AddBookmark.swift
+++ web-browser/iOS-web-browser/Bookmark/AddBookmark.swift
@@ -3,46 +3,62 @@ import SwiftUI
 struct AddBookmark: View {
     @ObservedObject var tabViewModel: TabViewModel
     @ObservedObject var bookmarkViewModel: BookmarkViewModel
+
     @State var bookmarkName: String
     @State var bookmarkURL: String
 
     @FocusState private var isNameFieldFocused: Bool
 
+    // MARK: - Body
+
     var body: some View {
         NavigationView {
             VStack(alignment: .leading, spacing: 20) {
-                HStack {
-                    VStack(alignment: .leading, spacing: 8) {
-                        TextField("Name", text: $bookmarkName)
-                            .textFieldStyle(.roundedBorder)
-                            .focused($isNameFieldFocused)
-                        TextField("URL", text: $bookmarkURL)
-                            .disabled(true)
-                            .textFieldStyle(.roundedBorder)
-                            .keyboardType(.URL)
-                    }
-                }
+                form
                 Spacer()
             }
             .padding()
             .navigationTitle("Add Bookmark")
             .navigationBarTitleDisplayMode(.inline)
-            .navigationBarItems(
-                leading: Button("Cancel", action: {
-                    tabViewModel.dismissAddBookmark()
-                }),
-                trailing: Button("Save", action: {
-                    tabViewModel.saveAndDismissAddBookmark(
-                        name: bookmarkName,
-                        url: bookmarkURL
-                    )
-                    bookmarkViewModel.delegate?.didTapAddBookmark(name: bookmarkName, urlString: bookmarkURL)
-                })
-            )
+            .navigationBarItems(leading: cancelButton, trailing: saveButton)
             .onAppear {
                 isNameFieldFocused = true
             }
             .background(Color(UIColor.systemGroupedBackground))
+        }
+    }
+
+    // MARK: - Buttons
+
+    var cancelButton: some View {
+        Button("Cancel", action: {
+            tabViewModel.dismissAddBookmark()
+        })
+    }
+
+    var saveButton: some View {
+        Button("Save", action: {
+            tabViewModel.saveAndDismissAddBookmark(
+                name: bookmarkName,
+                url: bookmarkURL
+            )
+            bookmarkViewModel.delegate?.didTapAddBookmark(name: bookmarkName, urlString: bookmarkURL)
+        })
+    }
+
+    // MARK: - Form
+
+    var form: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 8) {
+                TextField("Name", text: $bookmarkName)
+                    .textFieldStyle(.roundedBorder)
+                    .focused($isNameFieldFocused)
+                TextField("URL", text: $bookmarkURL)
+                    .disabled(true)
+                    .textFieldStyle(.roundedBorder)
+                    .keyboardType(.URL)
+            }
         }
     }
 }
