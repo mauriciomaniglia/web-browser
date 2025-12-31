5ccd8925548c976da7efd12d927343a96fca8861
mauriciomaniglia <manigliamauricio@gmail.com>
2025-12-31 15:23:28 -0300

    Refactor code structure for AddBookmark on iPadOS

--- web-browser/iPadOS-web-browser/Bookmark/AddBookmark.swift
+++ web-browser/iPadOS-web-browser/Bookmark/AddBookmark.swift
@@ -7,40 +7,66 @@ struct AddBookmark: View {
     @State var bookmarkName: String
     @State var bookmarkURL: String
 
+    // MARK: - Body
+
     var body: some View {
         VStack(spacing: 20) {
-            HStack {
-                Button(action: tabViewModel.dismissAddBookmark) {
-                    Image(systemName: "xmark")
-                }
-                Spacer()
-                Text("Add Bookmark")
-                    .font(.title2)
-                    .bold()
-                Spacer()
-                Button(action: {
-                    tabViewModel.saveAndDismissAddBookmark(
-                        name: bookmarkName,
-                        url: bookmarkURL
-                    )
-                    bookmarkViewModel.delegate?.didTapAddBookmark(name: bookmarkName, urlString: bookmarkURL)
-                }) {
-                    Text("Save")
-                }
-            }
-            HStack {
-                VStack(alignment: .leading, spacing: 5) {
-                    TextField("Title", text: $bookmarkName)
-                        .textFieldStyle(.roundedBorder)
-                    TextField("URL", text: $bookmarkURL)
-                        .textFieldStyle(.roundedBorder)
-                        .disabled(true)
-                }
-            }
+            header
+            form
             Spacer()
         }
         .padding()
         .frame(maxWidth: 400, maxHeight: 400)
         .background(Color(UIColor.systemBackground))
+    }
+
+    // MARK: - Header
+
+    var header: some View {
+        HStack {
+            closeButton
+            Spacer()
+            title
+            Spacer()
+            saveButton
+        }
+    }
+
+    var closeButton: some View {
+        Button(action: tabViewModel.dismissAddBookmark) {
+            Image(systemName: "xmark")
+        }
+    }
+
+    var title: some View {
+        Text("Add Bookmark")
+            .font(.title2)
+            .bold()
+    }
+
+    var saveButton: some View {
+        Button(action: {
+            tabViewModel.saveAndDismissAddBookmark(
+                name: bookmarkName,
+                url: bookmarkURL
+            )
+            bookmarkViewModel.delegate?.didTapAddBookmark(name: bookmarkName, urlString: bookmarkURL)
+        }) {
+            Text("Save")
+        }
+    }
+
+    // MARK: - Form
+
+    var form: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 5) {
+                TextField("Title", text: $bookmarkName)
+                    .textFieldStyle(.roundedBorder)
+                TextField("URL", text: $bookmarkURL)
+                    .textFieldStyle(.roundedBorder)
+                    .disabled(true)
+            }
+        }
     }
 }
