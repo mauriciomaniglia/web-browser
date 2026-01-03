574bd2bb16de8ce9f5ac27fa6cbea175b1e2b8e2
mauriciomaniglia <manigliamauricio@gmail.com>
2026-01-03 18:32:01 -0300

    Refactor code structure of NavigationBar

--- web-browser/web-browser/Shared/Window/Views/NavigationBar.swift
+++ web-browser/web-browser/Shared/Window/Views/NavigationBar.swift
@@ -3,32 +3,43 @@ import SwiftUI
 struct NavigationBar: View {
     @ObservedObject var viewModel: TabViewModel
 
+    // MARK: - Body
+
     var body: some View {
         HStack(spacing: 20) {
-            CustomGestureButton(imageName: "arrow.left",
-                                action: { viewModel.didTapBackButton?() },
-                                longPressAction: { viewModel.didLongPressBackButton?() }, 
-                                isDisabled: viewModel.isBackButtonDisabled)
-            .popover(isPresented: $viewModel.showBackList, arrowEdge: .bottom, content: {
-                NavigationList(didSelectPage: viewModel.didSelectBackListPage, pageList: viewModel.backList)
-            })
-            .onChange(of: viewModel.showBackList) { _, isPresented in
-                if !isPresented {
-                    viewModel.didDismissBackForwardPageList?()
-                }
-            }
+            backButton
+            forwardButton
+        }
+    }
 
-            CustomGestureButton(imageName: "arrow.right",
-                                action: { viewModel.didTapForwardButton?() },
-                                longPressAction: { viewModel.didLongPressForwardButton?() }, 
-                                isDisabled: viewModel.isForwardButtonDisabled)
-            .popover(isPresented: $viewModel.showForwardList, arrowEdge: .bottom, content: {
-                NavigationList(didSelectPage: viewModel.didSelectForwardListPage, pageList: viewModel.forwardList)
-            })
-            .onChange(of: viewModel.showForwardList) { _, isPresented in
-                if !isPresented {
-                    viewModel.didDismissBackForwardPageList?()
-                }
+    // MARK: - Buttons
+
+    var backButton: some View {
+        CustomGestureButton(imageName: "arrow.left",
+                            action: { viewModel.didTapBackButton?() },
+                            longPressAction: { viewModel.didLongPressBackButton?() },
+                            isDisabled: viewModel.isBackButtonDisabled)
+        .popover(isPresented: $viewModel.showBackList, arrowEdge: .bottom, content: {
+            NavigationList(didSelectPage: viewModel.didSelectBackListPage, pageList: viewModel.backList)
+        })
+        .onChange(of: viewModel.showBackList) { _, isPresented in
+            if !isPresented {
+                viewModel.didDismissBackForwardPageList?()
+            }
+        }
+    }
+
+    var forwardButton: some View {
+        CustomGestureButton(imageName: "arrow.right",
+                            action: { viewModel.didTapForwardButton?() },
+                            longPressAction: { viewModel.didLongPressForwardButton?() },
+                            isDisabled: viewModel.isForwardButtonDisabled)
+        .popover(isPresented: $viewModel.showForwardList, arrowEdge: .bottom, content: {
+            NavigationList(didSelectPage: viewModel.didSelectForwardListPage, pageList: viewModel.forwardList)
+        })
+        .onChange(of: viewModel.showForwardList) { _, isPresented in
+            if !isPresented {
+                viewModel.didDismissBackForwardPageList?()
             }
         }
     }
