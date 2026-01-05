c20a17f586383b3775a1b8901f8168d1c3e3a998
mauriciomaniglia <manigliamauricio@gmail.com>
2026-01-05 17:57:31 -0300

    Move WindowContent to its own file

---
+++ web-browser/iOS-web-browser/Window/WindowContent.swift
@@ -0,0 +1,119 @@
+import SwiftUI
+
+struct WindowContent: View {
+    @ObservedObject var tabViewModel: TabViewModel
+    @ObservedObject var historyViewModel: HistoryViewModel
+    @ObservedObject var bookmarkViewModel: BookmarkViewModel
+    @ObservedObject var searchSuggestionViewModel: SearchSuggestionViewModel
+    @ObservedObject var tabManager: TabManager
+
+    @State var isShowingSheet = false
+    @State private var isShowingTabManager = false
+
+    let webView: WebView
+
+    // MARK: - Body
+
+    var body: some View {
+        VStack {
+            addressBar
+            if shouldShowSearchSuggestions {
+                searchSuggestions
+            } else {
+                webViewFrame
+            }
+            bottomBar
+        }
+        .background(Color(.systemGray6))
+        .fullScreenCover(isPresented: $isShowingTabManager) {
+            tabManagerScreen
+        }
+        .popover(isPresented: $isShowingSheet, arrowEdge: .trailing, content: {
+            menuAlert
+        })
+        .popover(isPresented: $tabViewModel.showAddBookmark, arrowEdge: .trailing, content: {
+            addBookmarkAlert
+        })
+    }
+
+    // MARK: - Address Bar
+
+    var addressBar: some View {
+        AddressBarView(viewModel: tabViewModel, searchText: $tabViewModel.fullURL)
+    }
+
+    // MARK: - Search Suggestions
+
+    var shouldShowSearchSuggestions: Bool {
+        tabViewModel.showSearchSuggestions
+    }
+
+    var searchSuggestions: some View {
+        ScrollView {
+            SearchSuggestionView(viewModel: searchSuggestionViewModel)
+        }
+    }
+
+    // MARK: - WebView Frame
+
+    var webViewFrame: some View {
+        webView
+            .frame(maxWidth:.infinity, maxHeight: .infinity)
+            .opacity(tabViewModel.showWebView ? 1 : 0)
+    }
+
+    // MARK: - Bottom Bar
+
+    var bottomBar: some View {
+        HStack {
+            navigationButton
+            Spacer()
+            addNewTabButton
+            Spacer()
+            menuButton
+        }
+        .padding()
+    }
+
+    var navigationButton: some View {
+        NavigationBar(viewModel: tabViewModel)
+    }
+
+    var addNewTabButton: some View {
+        Button(action: { isShowingTabManager = true }) {
+            Image(systemName: "plus.square")
+        }
+    }
+
+    var menuButton: some View {
+        Button(action: { isShowingSheet.toggle() }) {
+            Image(systemName: "line.3.horizontal")
+        }
+    }
+
+    // MARK: - Tab Manager Screen
+
+    var tabManagerScreen: some View {
+        TabManagerScreen(tabManager: tabManager, isPresented: $isShowingTabManager)
+    }
+
+    // MARK: - Alerts
+
+    var menuAlert: some View {
+        Menu(
+            tabViewModel: tabViewModel,
+            bookmarkViewModel: bookmarkViewModel,
+            historyViewModel: historyViewModel,
+            isPresented: $isShowingSheet
+        )
+    }
+
+    var addBookmarkAlert: some View {
+        AddBookmark(
+            tabViewModel: tabViewModel,
+            bookmarkViewModel: bookmarkViewModel,
+            bookmarkName: tabViewModel.title,
+            bookmarkURL: tabViewModel.fullURL
+        )
+    }
+}
