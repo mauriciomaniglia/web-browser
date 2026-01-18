20a669f7db650ea199f2c8b3661fbad9ab773398
mauriciomaniglia <manigliamauricio@gmail.com>
2026-01-18 11:38:13 -0300

    Add new MenuCommand option to add a new tab on macOS

--- web-browser/web-browser/MacOS/Menu Commands/MenuCommands.swift
+++ web-browser/web-browser/MacOS/Menu Commands/MenuCommands.swift
@@ -4,6 +4,13 @@ struct MenuCommands: Commands {
     let tabBarManager: TabBarManager
 
     var body: some Commands {
+        CommandGroup(after: .newItem) {
+            Button("New Tab") {
+                tabBarManager.createNewTab()
+            }
+            .keyboardShortcut("t", modifiers: [.command])
+        }
+
         CommandMenu("Bookmarks") {
             Button("Add Bookmark...") {
                 tabBarManager.selectedTab?.tabViewModel.didTapAddBookmark()
