6f6a597fc682d41239229d338ff7a43fa0aba8ab
mauriciomaniglia <manigliamauricio@gmail.com>
2026-01-23 09:15:47 -0300

    Move BookmarkAdapter to its own file

---
+++ web-browser/web-browser/Shared/Bookmark/BookmarkAdapter.swift
@@ -0,0 +1,26 @@
+import Services
+
+class BookmarkAdapter {
+    weak var viewModel: BookmarkViewModel?
+    let manager: BookmarkManager
+
+    init(viewModel: BookmarkViewModel, manager: BookmarkManager) {
+        self.viewModel = viewModel
+        self.manager = manager
+    }
+
+    func didOpenBookmarkView() {
+        let presentableModels = manager.didOpenBookmarkView()
+        didUpdatePresentableModels(presentableModels)
+    }
+
+    func didSearchTerm(_ query: String) {
+        let presentableModels = manager.didSearchTerm(query)
+        didUpdatePresentableModels(presentableModels)
+    }
+
+    func didUpdatePresentableModels(_ models: [PresentableBookmark]) {
+        let bookmarks = models.map { BookmarkViewModel.Bookmark(id: $0.id, title: $0.title, url: $0.url) }
+        viewModel?.bookmarkList = bookmarks
+    }
+}
--- web-browser/web-browser/Shared/Bookmark/BookmarkComposer.swift
+++ web-browser/web-browser/Shared/Bookmark/BookmarkComposer.swift
@@ -44,28 +44,3 @@ extension BookmarkComposer: BookmarkViewModelDelegate {
         bookmarkStore.deletePages(withIDs: pagesID)
     }
 }
-
-class BookmarkAdapter {
-    weak var viewModel: BookmarkViewModel?
-    let manager: BookmarkManager
-
-    init(viewModel: BookmarkViewModel, manager: BookmarkManager) {
-        self.viewModel = viewModel
-        self.manager = manager
-    }
-
-    func didOpenBookmarkView() {
-        let presentableModels = manager.didOpenBookmarkView()
-        didUpdatePresentableModels(presentableModels)
-    }
-
-    func didSearchTerm(_ query: String) {
-        let presentableModels = manager.didSearchTerm(query)
-        didUpdatePresentableModels(presentableModels)
-    }
-
-    func didUpdatePresentableModels(_ models: [PresentableBookmark]) {
-        let bookmarks = models.map { BookmarkViewModel.Bookmark(id: $0.id, title: $0.title, url: $0.url) }
-        viewModel?.bookmarkList = bookmarks
-    }
-}
