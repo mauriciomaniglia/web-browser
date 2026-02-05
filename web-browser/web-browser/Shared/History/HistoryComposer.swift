b6254912ec273073201a19e7ef04d8e0d3f3f1ce
mauriciomaniglia <manigliamauricio@gmail.com>
2026-02-05 11:01:30 -0300

    Move HistoryAdapter to its own file

--- web-browser/web-browser/Shared/History/HistoryComposer.swift
+++ web-browser/web-browser/Shared/History/HistoryComposer.swift
@@ -44,34 +44,3 @@ extension HistoryComposer: HistoryViewModelDelegate {
         historyStore.deleteAllPages()
     }
 }
-
-class HistoryAdapter {
-    let viewModel: HistoryViewModel
-    let manager: HistoryManager
-
-    init(viewModel: HistoryViewModel, manager: HistoryManager) {
-        self.viewModel = viewModel
-        self.manager = manager
-    }
-
-    func didOpenHistoryView() {
-        let model = manager.didOpenHistoryView()
-        mapPresentableModel(model)
-    }
-
-    func didSearchTerm(_ query: String) {
-        let model = manager.didSearchTerm(query)
-        mapPresentableModel(model)
-    }
-
-    func mapPresentableModel(_ model: PresentableHistory) {
-        let history = model.list?.compactMap {
-            let pages = $0.pages.map {
-                HistoryViewModel.Page(id: $0.id, title: $0.title, url: $0.url)
-            }
-            return HistoryViewModel.Section(title: $0.title, pages: pages)
-        }
-
-        viewModel.historyList = history ?? []
-    }
-}
