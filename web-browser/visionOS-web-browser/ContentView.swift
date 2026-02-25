298e05b29f6d9eb43499b510dae2e67e97090bfc
mauriciomaniglia <manigliamauricio@gmail.com>
2026-02-25 18:01:51 -0300

    Create empty/default target for visionOS

---
+++ web-browser/visionOS-web-browser/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  visionOS-web-browser
+//
+//  Created by Mauricio Cesar on 25/02/26.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ web-browser/visionOS-web-browser/visionOS_web_browserApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionOS_web_browserApp.swift
+//  visionOS-web-browser
+//
+//  Created by Mauricio Cesar on 25/02/26.
+//
+
+import SwiftUI
+
+@main
+struct visionOS_web_browserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
